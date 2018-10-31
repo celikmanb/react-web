2dd551c961e42bb12c131b059e07cf4097d60c75
Burak Çelikman <celikmanb@mef.edu.tr>
2018-10-31 13:14:19 +0300

    web

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Main from './Components/Main.js';
 import './App.css';
 
 class App extends Component {
+  handleButtonPress = () => {
+      console.log('deneme');
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>TEDxHadi</h1>
+          <input type="text" name="text" placeholder="İsminiz" ></input>
+
+          <button className="btn orange" type="button" onKeyPress={ this.handleButtonPress } ><span>Hadi Başlayalım</span></button>
+
+
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return(
+      <header>
+        <h1>TEDxHadi</h1>
+        <input type="text" name="text" placeholder="İsminiz" ></input>
+      </header>
+    );
+  }
+}
+
+export default Main;
